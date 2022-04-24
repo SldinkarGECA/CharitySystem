90b797bea3f97fa3628630637520a9eff6f29cca
SldinkarGECA <sldinkar@gmail.com>
2022-04-24 16:32:08 +0530

    hosting on react

--- charity-react/src/web3.js
+++ charity-react/src/web3.js
@@ -1,6 +1,11 @@
 const Web3 = require("web3");
-window.ethereum.request({ method: "eth_requestAccounts" });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const web3 = new Web3(window.ethereum);
-// const web3 = new Web3();
+const provider = new HDWalletProvider(
+  "grunt scan gravity boring cruise exhaust demise smile zone easily denial forest",
+  "https://rinkeby.infura.io/v3/dd3d6cdd436d4a42892d62c808f35b61"
+);
+
+const web3 = new Web3(provider);
+
 export default web3;
