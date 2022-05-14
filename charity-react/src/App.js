0f22f6cdd00c5fa011058c02faae9cee4849cdd9
Sudarshan Gosavi <sudarshangosavi17@gmail.com>
2022-05-14 11:50:15 +0530

    Changing navbar

--- charity-react/src/App.js
+++ charity-react/src/App.js
@@ -1,26 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-
-import CharityProjects from "./components/CharityProjects";
-import Products from "./components/Products";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Stores from "./components/Stores";
-import Donors from "./components/Donors";
 
 function App() {
   return (
     <div className="container-fluid">
-      <Navbar>
-        <Routes>
-          <Route path="" element={<Home />} />
-          <Route exact path="/CharityProjects" element={<CharityProjects />} />
-          <Route exact path="/donors" element={<Donors />} />
-          <Route exact path="/products" element={<Products />} />
-          <Route exact path="/stores" element={<Stores />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Navbar>
+      <Navbar />
       <Footer />
     </div>
   );
