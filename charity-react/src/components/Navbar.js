10af052f920a2f4dc8b7fb4f85bfc4db9beaf5f4
SldinkarGECA <sldinkar@gmail.com>
2022-05-14 19:04:21 +0530

    404 error

--- charity-react/src/components/Navbar.js
+++ charity-react/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Routes } from "react-router-dom";
 import CharityProjects from "./CharityProjects";
 import Products from "./Products";
 import Home from "./Home";
